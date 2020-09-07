ae21e8473b28ca5b5d25ece39b707e9a36863dc5
stevewilkinghof <22236093+stevewilkinghoff@users.noreply.github.com>
2020-09-07 04:29:13 -0600

    cleaned

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://SteveAdmin:Gr33n7pple5!!@cluster0-shard-00-00.asquc.mongodb.net:27017,cluster0-shard-00-01.asquc.mongodb.net:27017,cluster0-shard-00-02.asquc.mongodb.net:27017/cottagedata?ssl=true&replicaSet=atlas-ppexg2-shard-0&authSource=admin&retryWrites=true&w=majority',
-
-  };
